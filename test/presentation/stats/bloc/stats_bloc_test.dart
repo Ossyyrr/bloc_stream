f79b9a43d2e7379c07932e472d6807e15678a199
Patricia Manzanero <patricia.manzanero-jimenez@capgemini.com>
2024-07-29 12:10:25 +0200

    test [wip]

---
+++ test/presentation/stats/bloc/stats_bloc_test.dart
@@ -0,0 +1,15 @@
+// import 'package:test/test.dart'; 
+// import 'package:bloc_stream/presentation/stats/bloc/stats_bloc.dart';
+
+// void main() {
+// 	group(
+// 		stats_bloc_test, 
+// 		() {
+// 			test(
+// 				'',
+// 				() async {
+// 				},
+// 			);
+// 		},
+// 	);
+// }
